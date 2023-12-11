23d0e82588859b3f14e9e9b1d3e3c0b0d3dfd996
Ken Kozma <kenkozma17@gmail.com>
2023-12-11 08:38:20 +0800

    Forgot to change verifycsrftoken value

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/booking-payment-success-test'
+        '/booking-payment-success'
     ];
 }
