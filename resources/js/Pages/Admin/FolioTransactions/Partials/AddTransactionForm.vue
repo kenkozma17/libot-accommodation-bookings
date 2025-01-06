e771f9869824c148751646b3ea52b575a58e147d
kenkozma17 <kenkozma17@gmail.com>
2025-01-06 08:58:13 +0800

    Adding adjustment service

--- resources/js/Pages/Admin/FolioTransactions/Partials/AddTransactionForm.vue
+++ resources/js/Pages/Admin/FolioTransactions/Partials/AddTransactionForm.vue
@@ -25,7 +25,7 @@ const form = useForm({
 });
 
 const isManualPayment = computed(() => {
-    return form.service.slug === 'down-payment' || form.service.slug === 'manual-payment';
+    return form.service.slug === 'down-payment' || form.service.slug === 'manual-payment' || form.service.slug === 'adjustment';
 });
 
 const createFolioTransaction = () => {
